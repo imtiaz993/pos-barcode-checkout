dddda2c6fb23b6a5d30fbd36e609016388e261a9
imtiaz993 <imtiazh993@gmail.com>
2024-11-18 11:50:06 +0500

    reverted beep test

--- app/BarcodeScanner.tsx
+++ app/BarcodeScanner.tsx
@@ -1,34 +1,29 @@
 import React, { useEffect, useRef } from "react";
 import { BrowserMultiFormatReader } from "@zxing/browser";
 
-const ZXingScanner = ({ onScan, onError }: any) => {
+const ZXingScanner = ({ onScan, onError }:any) => {
   const videoRef = useRef<any>(null);
-  // const beepSound = new Audio("/beep.mp3");
-  const audioRef = useRef<any>(null);
-  const beepSound = () => {
-    if (audioRef.current) {
-      audioRef.current.play().catch((error: any) => {
-        console.error("Audio playback failed:", error);
-      });
-    }
-  };
+  const beepSound = new Audio("/beep.mp3");
 
   useEffect(() => {
     const codeReader = new BrowserMultiFormatReader();
 
-    codeReader.decodeFromVideoDevice("", videoRef.current, (result, error) => {
-      if (result) {
-        onScan(result.getText(), beepSound);
+    codeReader.decodeFromVideoDevice(
+      '',
+      videoRef.current,
+      (result, error) => {
+        if (result) {
+          onScan(result.getText(), beepSound);
+        }
+        if (error) {
+          onError && onError(error);
+        }
       }
-      if (error) {
-        onError && onError(error);
-      }
-    });
+    );
   }, [onScan, onError]);
 
   return (
     <div className="h-80">
-      <audio ref={audioRef} src="/beep.mp3" preload="auto" />
       <video ref={videoRef} style={{ width: "100%", height: "100%" }} />
     </div>
   );
