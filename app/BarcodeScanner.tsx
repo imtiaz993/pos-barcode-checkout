f8a1a6872d8feecd051aa568d3381cb9df05b249
imtiaz993 <imtiazh993@gmail.com>
2024-11-20 10:37:04 +0500

    added all formats

--- app/BarcodeScanner.tsx
+++ app/BarcodeScanner.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { BrowserMultiFormatReader } from "@zxing/browser";
 import { DecodeHintType, BarcodeFormat } from "@zxing/library";
 
-const ZXingScanner = ({ onScan, onError }:any) => {
+const ZXingScanner = ({ onScan, onError }: any) => {
   const videoRef = useRef<any>(null);
   const beepSound = new Audio("/beep.mp3");
 
@@ -14,22 +14,35 @@ const ZXingScanner = ({ onScan, onError }:any) => {
       BarcodeFormat.CODE_39,
       BarcodeFormat.EAN_13,
       BarcodeFormat.EAN_8,
+      BarcodeFormat.AZTEC,
+      BarcodeFormat.CODABAR,
+      BarcodeFormat.CODE_39,
+      BarcodeFormat.CODE_93,
+      BarcodeFormat.CODE_128,
+      BarcodeFormat.DATA_MATRIX,
+      BarcodeFormat.EAN_8,
+      BarcodeFormat.EAN_13,
+      BarcodeFormat.ITF,
+      BarcodeFormat.MAXICODE,
+      BarcodeFormat.PDF_417,
+      BarcodeFormat.QR_CODE,
+      BarcodeFormat.RSS_14,
+      BarcodeFormat.RSS_EXPANDED,
+      BarcodeFormat.UPC_A,
+      BarcodeFormat.UPC_E,
+      BarcodeFormat.UPC_EAN_EXTENSION,
       // Include other formats if needed
     ]);
 
     const codeReader = new BrowserMultiFormatReader(hints);
 
-    codeReader.decodeFromVideoDevice(
-      '',
-      videoRef.current,
-      (result, error) => {
-        if (result) {
-          onScan(result.getText(), beepSound);
-        } else if (error) {
-          onError && onError(error);
-        }
+    codeReader.decodeFromVideoDevice("", videoRef.current, (result, error) => {
+      if (result) {
+        onScan(result.getText(), beepSound);
+      } else if (error) {
+        onError && onError(error);
       }
-    );
+    });
   }, [onScan, onError]);
 
   return (
