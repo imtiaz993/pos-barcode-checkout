0b0c937ccc9856aaed035e10aa2379efa0a2749e
imtiaz993 <imtiazh993@gmail.com>
2024-11-20 10:42:51 +0500

    removed formats but kept try harder

--- app/BarcodeScanner.tsx
+++ app/BarcodeScanner.tsx
@@ -9,30 +9,30 @@ const ZXingScanner = ({ onScan, onError }: any) => {
   useEffect(() => {
     const hints = new Map();
     hints.set(DecodeHintType.TRY_HARDER, true);
-    hints.set(DecodeHintType.POSSIBLE_FORMATS, [
-      BarcodeFormat.CODE_128,
-      BarcodeFormat.CODE_39,
-      BarcodeFormat.EAN_13,
-      BarcodeFormat.EAN_8,
-      BarcodeFormat.AZTEC,
-      BarcodeFormat.CODABAR,
-      BarcodeFormat.CODE_39,
-      BarcodeFormat.CODE_93,
-      BarcodeFormat.CODE_128,
-      BarcodeFormat.DATA_MATRIX,
-      BarcodeFormat.EAN_8,
-      BarcodeFormat.EAN_13,
-      BarcodeFormat.ITF,
-      BarcodeFormat.MAXICODE,
-      BarcodeFormat.PDF_417,
-      BarcodeFormat.QR_CODE,
-      BarcodeFormat.RSS_14,
-      BarcodeFormat.RSS_EXPANDED,
-      BarcodeFormat.UPC_A,
-      BarcodeFormat.UPC_E,
-      BarcodeFormat.UPC_EAN_EXTENSION,
-      // Include other formats if needed
-    ]);
+    // hints.set(DecodeHintType.POSSIBLE_FORMATS, [
+    //   BarcodeFormat.CODE_128,
+    //   BarcodeFormat.CODE_39,
+    //   BarcodeFormat.EAN_13,
+    //   BarcodeFormat.EAN_8,
+    //   BarcodeFormat.AZTEC,
+    //   BarcodeFormat.CODABAR,
+    //   BarcodeFormat.CODE_39,
+    //   BarcodeFormat.CODE_93,
+    //   BarcodeFormat.CODE_128,
+    //   BarcodeFormat.DATA_MATRIX,
+    //   BarcodeFormat.EAN_8,
+    //   BarcodeFormat.EAN_13,
+    //   BarcodeFormat.ITF,
+    //   BarcodeFormat.MAXICODE,
+    //   BarcodeFormat.PDF_417,
+    //   BarcodeFormat.QR_CODE,
+    //   BarcodeFormat.RSS_14,
+    //   BarcodeFormat.RSS_EXPANDED,
+    //   BarcodeFormat.UPC_A,
+    //   BarcodeFormat.UPC_E,
+    //   BarcodeFormat.UPC_EAN_EXTENSION,
+    //   // Include other formats if needed
+    // ]);
 
     const codeReader = new BrowserMultiFormatReader(hints);
 
