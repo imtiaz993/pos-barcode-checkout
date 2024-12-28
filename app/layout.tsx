3f737548579fb7db2c4fee739cf8ffdabe247da2
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2024-12-28 12:49:11 +0500

    fixed spacing

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1.0, user-scalable=no"
         ></meta>
       </head>
-      <body className="max-w-md mx-auto">
+      <body className="max-w-md mx-auto py-4">
         <Suspense>{children}</Suspense>
         <Toaster />
       </body>
