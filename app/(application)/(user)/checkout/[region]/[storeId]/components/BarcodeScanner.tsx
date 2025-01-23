b74ff9a6def4d4d71e628dc9108e5cca5d978127
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-01-23 19:08:59 +0500

    added camera error

--- app/(application)/(user)/checkout/[region]/[storeId]/components/BarcodeScanner.tsx
+++ app/(application)/(user)/checkout/[region]/[storeId]/components/BarcodeScanner.tsx
@@ -1,22 +1,47 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BrowserMultiFormatReader } from "@zxing/browser";
 
 const ZXingScanner = ({ onScan, openCart }: any) => {
   const videoRef = useRef<any>(null);
   const beepSound = new Audio("/beep.mp3");
+  const [cameraAccessError, setCameraAccessError] = useState(false);
 
   useEffect(() => {
     const codeReader = new BrowserMultiFormatReader();
 
-    codeReader.decodeFromVideoDevice("", videoRef.current, (result, error) => {
-      if (result && !openCart) {
-        onScan(result.getText(), beepSound);
-      }
-    });
+    codeReader
+      .decodeFromVideoDevice("", videoRef.current, (result) => {
+        if (result && !openCart) {
+          onScan(result.getText(), beepSound);
+        }
+      })
+      .catch((error) => {
+        setCameraAccessError(true);
+      });
   }, [onScan]);
+
+  const handleReload = () => {
+    window.location.reload();
+  };
 
   return (
     <div className="h-44 w-3/4 mx-auto relative border-2 border-white">
+      {cameraAccessError && (
+        <div className="text-red-500">
+          Camera access denied.{" "}
+          <span className="text-blue-500 cursor-pointer" onClick={handleReload}>
+            Reload page{" "}
+          </span>
+          or{" "}
+          <a
+            href="your-permission-settings-url"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            check permissions.
+          </a>
+        </div>
+      )}
       <video
         ref={videoRef}
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
