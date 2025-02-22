f135eadb3b895ca139c89773e666995d125b02af
Imtiaz Hussain <57477671+imtiaz993@users.noreply.github.com>
2025-02-22 20:44:53 +0500

    updated icon

--- app/(application)/admin/(dashboard)/manage-users/page.tsx
+++ app/(application)/admin/(dashboard)/manage-users/page.tsx
@@ -4,7 +4,12 @@ import Loader from "@/components/loader";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import { FaUser, FaPhoneAlt, FaCalendarAlt, FaSignInAlt } from "react-icons/fa";
+import {
+  FaPhoneAlt,
+  FaCalendarAlt,
+  FaSignInAlt,
+  FaUsers,
+} from "react-icons/fa";
 
 const Page = () => {
   const [users, setUsers] = useState<any[]>([]);
@@ -32,7 +37,7 @@ const Page = () => {
       <div className="min-h-[calc(100dvh-60px-16px)] mx-auto px-2 sm:px-4 py-2">
         <div className="w-full mx-auto mt-3 sm:mt-4 flex flex-col min-h-[calc(100dvh-82px)]">
           <h1 className="mb-4 sm:mb-6 text-lg sm:text-2xl font-semibold text-gray-800 flex items-center gap-2">
-            <FaUser /> All Users
+            <FaUsers /> All Users
           </h1>
 
           <div className="bg-white shadow-lg rounded-lg flex-grow flex flex-col overflow-hidden">
